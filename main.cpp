31c301035e73c0f0855cdff50f721f7dc85a2987
MondoQuintana <mondoquintana@unm.edu>
2024-11-18 13:38:04 -0700

    V0.2 Sum Working, Working on Max and Min

--- main.cpp
+++ main.cpp
@@ -30,32 +30,80 @@ public:
             ptr->next=t;
         }
     }
-    item getItem(int searchItem){
+    int getSize(){
         struct vec *ptr=begin;
-        while (ptr!=NULL)
+        int i = 0;
+        while (ptr!=NULL){
+            i++;
+            ptr=ptr->next;
+        }
+        return i;
+    }
+    bool sizeTest(int a){
+        bool test;
+        if (a>getSize())
         {
-            for (int i = 0; i < searchItem; i++)
-            {
+            test=true;
+        }
+        else{
+            test=false;
+        }
+        return test;
+    }
+    item getItem(int searchItem){
+        if(sizeTest(searchItem)==1){
+            struct vec *ptr=begin;
+            return ptr->val;
+        }
+        else{
+            struct vec *ptr=begin;
+            for (int i = 0; i < searchItem; i++){
                 ptr=ptr->next;
             }
             return ptr->val;
         }
-        return ptr->val;
+    }
+    item getSum(){
+        item sum=0;
+        struct vec *ptr=begin;
+        while (ptr!=NULL)
+        {
+            sum+=ptr->val;
+            ptr=ptr->next;
+        }
+        
+        return sum;
+    }
+    item getMax(){
+        item max;
+        struct vec *ptr=begin;
+        max=ptr->val;
+        while (ptr!=NULL)
+        {
+            ptr=ptr->next;
+            if (ptr->val>=max)
+            {
+                max=ptr->val;
+            }
+            else{
+                max=max;
+            }
+        }
+        return max;
     }
     ~myVector(){delete begin;}
 };
 
 int main(){
     myVector<double> test;
-    
-    for (int i = 1; i < 11; i++)
-    {
+    for (int i = 1; i < 11; i++){
         test.addItem(i);
     }
-    for (int i = 0; i < 20; i++)
-    {
+    for (int i = 0; i < 10; i++){
         cout<<test.getItem(i)<<endl;
     }
-    
+    cout<<"Size of myVector = "<<test.getSize()<<endl;
+    cout<<test.getSum()<<endl;
+    cout<<test.getMax()<<endl;
     return 0;
 }
